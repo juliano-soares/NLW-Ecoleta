29211d8c6606c5cf52abe8c3986586a27dd5a8fd
juliano-soares <37117169+juliano-soares@users.noreply.github.com>
2020-06-11 22:18:56 -0300

    Add files via upload

---
+++ nlw/src/database/db.js
@@ -0,0 +1,64 @@
+const sqlite3 = require("sqlite3").verbose()
+
+const db = new sqlite3.Database("./src/database/database.db") 
+
+module.exports = db
+
+// db.serialize(() => {
+//   // db.run(`
+//   //   CREATE TABLE IF NOT EXISTS places (
+//   //     id INTEGER PRIMARY KEY AUTOINCREMENT,
+//   //     image TEXT,
+//   //     name TEXT,
+//   //     address TEXT,
+//   //     address2 TEXT,
+//   //     state TEXT,
+//   //     city TEXT,
+//   //     items TEXT
+//   //   );
+//   // `)
+  
+//   // const query = `
+//   //   INSERT INTO places (
+//   //     image,
+//   //     name,
+//   //     address,
+//   //     address2,
+//   //     state,
+//   //     city,
+//   //     items
+//   //   ) VALUES (?,?,?,?,?,?,?);
+//   // `
+//   // const values = [
+//   //     "image",
+//   //     "name",
+//   //     "address",
+//   //     "address2",
+//   //     "state",
+//   //     "city",
+//   //     "items" 
+//   // ]
+
+//   // function afterInsertData(err) {
+//   //   if(err) {
+//   //     return console.log(err)
+//   //   }
+
+//   //   console.log("Cadastrado com sucesso");
+//   // }
+//   // db.run(query, values, afterInsertData)
+  
+//   // db.all(`SELECT * FROM places`, function (err, rows) {
+//   //   if (err) {
+//   //     console.log(err)
+//   //   }
+//   //   console.log(rows);  
+//   // })
+
+//   // db.run(`DELETE FROM places WHERE id = ?`,[1], function (err) {
+//   //  if (err) {
+//   //    console.log(err)
+//   //  }
+//   //  console.log("Registro deletado com sucesso!");
+//   // })
+// })
